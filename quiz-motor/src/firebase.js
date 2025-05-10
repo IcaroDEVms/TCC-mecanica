85fc810f04531c99ef03664b76a709b84cf2295e
trindadej44 <141645033+trindadej44@users.noreply.github.com>
2025-05-10 16:35:21 -0300

    add firebase

---
+++ quiz-motor/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC721Md9sNnwEIPpM6Oc35pIMWf1uOP6UQ",
+  authDomain: "quiz-motor.firebaseapp.com",
+  projectId: "quiz-motor",
+  storageBucket: "quiz-motor.firebasestorage.app",
+  messagingSenderId: "1012731523830",
+  appId: "1:1012731523830:web:7e148dece2576bacdcb2d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
